4566aede333035b4bf36541e78432b17bb588260
Alan Simon <alancarlsimon@gmail.com>
2020-12-12 02:16:10 -0500

    Day 8 part 2

--- src/day8/day8.kt
+++ src/day8/day8.kt
@@ -54,17 +54,21 @@ Run your copy of the boot code. Immediately before any instruction is executed a
 
 // increase the global var
 const val ACC = "acc"
+
 // jump above/below by the value
 const val JMP = "jmp"
+
 // go to next line
 const val NOP = "nop"
 
 var accumulator = 0
-var lineNumber = 0;
+var lineNumber = 0
+var invertInstruction = true
 
 fun main() {
-    runTest()
-    runPart1()
+//    runTest()
+//    runPart1()
+    runPart2()
 }
 
 //Build a list from the input
@@ -82,8 +86,15 @@ fun runPart1() {
     processInput(input)
 }
 
+fun runPart2() {
+    println("Part 2 ------------------------")
+    val input = File("src/day8/input").readLines()
+
+    processInputPart2(input)
+}
+
 fun processInput(input: List<String>) {
-    val processedInstructions = Array(input.size) {""}
+    val processedInstructions = Array(input.size) { "" }
     var noRepeatedInstructions = true
     //Reset for any subsequent run
 
@@ -98,11 +109,10 @@ fun processInput(input: List<String>) {
         if (processedInstructions[lineNumber] == line) {
             noRepeatedInstructions = false
             println("Accumulator is: $accumulator")
-
         }
 
         //break down line
-        val instruction =  line.substring(0..2)
+        val instruction = line.substring(0..2)
         val sign = line.substring(4..4)
         val value = line.substring(5 until line.length)
 
@@ -116,7 +126,7 @@ fun processInput(input: List<String>) {
 }
 
 fun performInstruction(instruction: String, sign: String, value: String) {
-    when(instruction) {
+    when (instruction) {
         ACC -> {
             when (sign) {
                 "+" -> {
@@ -141,6 +151,68 @@ fun performInstruction(instruction: String, sign: String, value: String) {
         NOP -> {
             //NOOP
             lineNumber++
+        }
+    }
+}
+
+
+fun processInputPart2(input: List<String>) {
+    var stillInfiniteLoop = true
+    val testedJmpAndNopInstructions = Array(input.size) { "" }
+
+    while (stillInfiniteLoop) {
+        val processedInstructions = Array(input.size) { "" }
+        var noRepeatedInstructionsLoop = true
+        val cleanList = input.toMutableList()
+        //Reset for any subsequent run
+        accumulator = 0
+        lineNumber = 0
+        invertInstruction = true
+
+        while (noRepeatedInstructionsLoop) {
+            //get line
+            var line = cleanList[lineNumber]
+            //println(line)
+
+            //break down line
+            var instruction = line.substring(0..2)
+            var sign = line.substring(4..4)
+            var value = line.substring(5 until line.length)
+
+            //if this line is a nop/jmp that should change, do it now
+            if((instruction == "nop" || instruction == "jmp") && invertInstruction && testedJmpAndNopInstructions[lineNumber] != line ) {
+
+                testedJmpAndNopInstructions[lineNumber] = line
+
+                if (instruction == "nop") {
+                    line = line.replace("nop", "jmp")
+                    instruction = instruction.replace("nop", "jmp")
+                } else {
+                    line = line.replace("jmp", "nop")
+                    instruction = instruction.replace("jmp", "nop")
+                }
+                cleanList[lineNumber] = line
+                invertInstruction = false
+                println("Line was modified $line")
+            }
+
+            //check if instruction is in processed list
+            if (processedInstructions[lineNumber] == line) {
+                noRepeatedInstructionsLoop = false
+                println("Accumulator is: $accumulator")
+            } else {
+                //add instruction to processed list
+                processedInstructions[lineNumber] = line
+
+                //perform instruction
+                performInstruction(instruction, sign, value)
+
+                if (lineNumber == processedInstructions.size) {
+                    println(accumulator)
+                    noRepeatedInstructionsLoop = false
+                    stillInfiniteLoop = false
+                }
+            }
         }
     }
 }
